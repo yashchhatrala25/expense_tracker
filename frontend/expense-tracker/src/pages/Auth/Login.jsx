33f86adb9da2e9999e3842abdff6c4f73b242fe9
yashchhatrala25 <yashchhatrala25@gmail.com>
2025-05-08 18:51:43 +0530

    expense tracker project using mern stack

---
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>Please enter your details to log in</p>
+      </div>
+    </div>
+  )
+}
+
+export default Login
